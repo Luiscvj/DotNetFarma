a41835a041ecf3ee7164073a7cc9d6ddc9374962
Luiscvj <luiscarlos.villanjurado@gmail.com>
2023-09-19 22:22:52 -0500

    feat: :sparkles: Se agrego el dataseeding para los primeros registros de la base de datos

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -5,6 +5,8 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[ApiVersion("1.0")]
+[ApiVersion("1.1")]
 
 public class BaseApiController : ControllerBase
 {
---
+++ Persistence/DataSeeding/SeedingInitial.cs
@@ -0,0 +1,90 @@
+
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Persistence.DataSeeding;
+
+    public class SeedingInitial
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+
+
+        {
+            // Proveedores
+            var ProveedorA = new Proveedor { ProveedorId =1, Nombre="ProveedorA",Telefono ="032238492",Email="ProveedorA@gmail.com",Direccion="Cra 19 # 839"};
+            var ProveedorB = new Proveedor { ProveedorId = 2, Nombre = "ProveedorB", Telefono = "032238493", Email = "ProveedorB@gmail.com",Direccion="Cra 22 # 839" };
+            var ProveedorC = new Proveedor { ProveedorId = 3, Nombre = "ProveedorC", Telefono = "032238494", Email = "ProveedorC@gmail.com" ,Direccion="Calle 14 # 839"};
+           
+            // Medicamentos
+            var Paracetamol = new Medicamento {MedicamentoId =1 , Nombre = "Paracetamol", Precio=2500, Stock =150, FechaExpiracion=new DateTime(2024,06,15) , ProveedorId= ProveedorA.ProveedorId};
+            var Ibuprofeno = new Medicamento { MedicamentoId = 2, Nombre = "Ibuprofeno", Precio = 3000, Stock = 100, FechaExpiracion = new DateTime( 2023,08, 15), ProveedorId = ProveedorB.ProveedorId };
+            var Aspirina = new Medicamento { MedicamentoId = 3, Nombre = "Aspirina", Precio = 2000, Stock = 75, FechaExpiracion = new DateTime(2024,12, 20), ProveedorId = ProveedorC.ProveedorId };
+
+            // Compras
+            var Compra1 = new Compra {CompraId= 1, FechaCompra = new DateTime(2001,01,01),ProveedorId = ProveedorA.ProveedorId,Cantidad=200,Precio=200000,MedicamentoId =Paracetamol.MedicamentoId};
+            var Compra2 = new Compra { CompraId = 2, FechaCompra = new DateTime( 2022,03, 15), ProveedorId = ProveedorB.ProveedorId, Cantidad = 150, Precio = 180000, MedicamentoId = Ibuprofeno.MedicamentoId };
+            var Compra3 = new Compra { CompraId = 3, FechaCompra = new DateTime( 2023,10, 06), ProveedorId = ProveedorC.ProveedorId, Cantidad = 100, Precio = 150000, MedicamentoId = Aspirina.MedicamentoId };
+
+            // Pacientes
+            var Paciente1 = new Paciente {PacienteId =1 , Nombre ="Sofia",Apellidos ="Alvarez",Direccion="Cra 19 #8-45 Barrio Comuneros", Telefono="3224243429"};
+            var Paciente2 = new Paciente { PacienteId = 2, Nombre = "Carlos", Apellidos = "Gomez", Direccion = "Cra 25 #12-34 Barrio Los Pinos", Telefono = "3225556677" };
+            var Paciente3 = new Paciente { PacienteId = 3, Nombre = "Laura", Apellidos = "Ramirez", Direccion = "Av. 4 #9-56 Barrio San Martin", Telefono = "3117778899" };
+
+            // Cargos
+            var Cargo1  = new Cargo {CargoId = 1, Nombre="Auxiliar de farmacia",Descripcion="Se encarga de atender en la farmacia"};
+            var Cargo2 = new Cargo { CargoId = 2, Nombre = "Enfermera", Descripcion = "Brinda cuidados médicos a los pacientes" };
+            var Cargo3 = new Cargo { CargoId = 3, Nombre = "Contador", Descripcion = "Maneja las finanzas de la empresa" };
+
+            // Países
+            var Pais1 = new Pais {PaisId =1, Nombre ="Colombia"};
+            var Pais2 = new Pais { PaisId = 2, Nombre = "Perú" };
+            var Pais3 = new Pais { PaisId = 3, Nombre = "Argentina" };
+
+            // Departamentos
+            var Departamento1 = new Departamento {DepartamentoId =1 , Nombre="Santander", PaisId= Pais1.PaisId};
+            var Departamento2 = new Departamento { DepartamentoId = 2, Nombre = "Lima", PaisId = Pais2.PaisId };
+            var Departamento3 = new Departamento { DepartamentoId = 3, Nombre = "Buenos Aires", PaisId = Pais3.PaisId };
+
+            // Ciudades
+            var Ciudad1 = new Ciudad {CiudadId = 1, Nombre ="Bucarmanga",DepartamentoId = Departamento1.DepartamentoId};
+            var Ciudad2 = new Ciudad { CiudadId = 2, Nombre = "Lima", DepartamentoId = Departamento2.DepartamentoId };
+            var Ciudad3 = new Ciudad { CiudadId = 3, Nombre = "Buenos Aires", DepartamentoId = Departamento3.DepartamentoId };
+
+            // Arls
+            var Arl1 = new Arl {ArlId = 1, Nombre ="Cajasan", Telefono ="60793820", Email ="CajasanAyuda@gmail.com",Direccion ="calle 14 # 24-10"};
+            var Arl2 = new Arl { ArlId = 2, Nombre = "SaludTotal", Telefono = "601234567", Email = "contacto@saludtotal.com", Direccion = "Calle 10 #30-45" };
+            var Arl3 = new Arl { ArlId = 3, Nombre = "Cafesalud", Telefono = "601112233", Email = "info@cafesalud.com", Direccion = "Av. 5 #18-22" };
+
+            // Eps
+            var Eps1 = new Eps {EpsId = 1, Nombre = "Avanzar",Telefono ="60783822", Email= "AtencionAlCliente@avanzar.co", Direccion ="Calle 49 #43-2"};
+            var Eps2 = new Eps { EpsId = 2, Nombre = "Coomeva", Telefono = "601987654", Email = "atencion@coomeva.com", Direccion = "Cra 15 #22-10" };
+            var Eps3 = new Eps { EpsId = 3, Nombre = "Famisanar", Telefono = "601876543", Email = "info@famisanar.com", Direccion = "Av. 8 #12-30" };
+
+            // Empleados
+            var Empleado1 = new Empleado {EmpleadoId = 1, CargoId = Cargo1.CargoId, Nombres ="Jorge", Apellidos="Escalante",Direccion ="Cra 33 #48-3",Telefono ="3294902231",FechaContratacion = new DateTime(2011,02,04), CiudadId= Ciudad1.CiudadId,ArlId= Arl1.ArlId, EpsId= Eps1.EpsId};
+            var Empleado2 = new Empleado { EmpleadoId = 2, CargoId = Cargo2.CargoId, Nombres = "María", Apellidos = "López", Direccion = "Cra 18 #45-6", Telefono = "3209876543", FechaContratacion = new DateTime( 2015,10, 09), CiudadId = Ciudad2.CiudadId, ArlId = Arl2.ArlId, EpsId = Eps2.EpsId };
+            var Empleado3 = new Empleado { EmpleadoId = 3, CargoId = Cargo3.CargoId, Nombres = "Juan", Apellidos = "Perez", Direccion = "Av. 3 #8-15", Telefono = "3101234567", FechaContratacion = new DateTime(2019,05, 07), CiudadId = Ciudad3.CiudadId, ArlId = Arl3.ArlId, EpsId = Eps3.EpsId };
+
+            // Ventas
+            var Venta1 = new Venta {VentaId =1 , FechaVenta = new DateTime(2023,02,10),EmpleadoId = Empleado1.EmpleadoId, PacienteId = Paciente1.PacienteId, Cantidad = 2, Precio= 2200 , MedicamentoId= Paracetamol.MedicamentoId};
+            var Venta2 = new Venta { VentaId = 2, FechaVenta = new DateTime( 2023,04, 18), EmpleadoId = Empleado2.EmpleadoId, PacienteId = Paciente2.PacienteId, Cantidad = 3, Precio = 2700, MedicamentoId = Ibuprofeno.MedicamentoId };
+            var Venta3 = new Venta { VentaId = 3, FechaVenta = new DateTime(2023,07, 22 ), EmpleadoId = Empleado3.EmpleadoId, PacienteId = Paciente3.PacienteId, Cantidad = 1, Precio = 1500, MedicamentoId = Aspirina.MedicamentoId };
+
+     
+
+            //Insercion de los datos,
+            modelBuilder.Entity<Proveedor>().HasData(ProveedorA, ProveedorB, ProveedorC);
+            modelBuilder.Entity<Medicamento>().HasData(Paracetamol, Aspirina, Ibuprofeno);
+            modelBuilder.Entity<Compra>().HasData(Compra1,Compra2, Compra3);
+            modelBuilder.Entity <Paciente>().HasData(Paciente1, Paciente2,Paciente3);
+            modelBuilder.Entity<Cargo>().HasData(Cargo1,Cargo2, Cargo3);
+            modelBuilder.Entity <Pais>().HasData(Pais1, Pais2,Pais3);
+            modelBuilder.Entity<Departamento>().HasData(Departamento1,Departamento2, Departamento3);
+            modelBuilder.Entity <Ciudad>().HasData(Ciudad1, Ciudad2,Ciudad3);
+            modelBuilder.Entity<Arl>().HasData(Arl1,Arl2, Arl3);
+            modelBuilder.Entity <Eps>().HasData(Eps1, Eps2,Eps3);
+            modelBuilder.Entity<Empleado>().HasData(Empleado1,Empleado2, Empleado3);
+            modelBuilder.Entity <Venta>().HasData(Venta1, Venta2,Venta3);
+        }
+    }
